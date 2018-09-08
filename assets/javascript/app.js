8d2670ffe928e70fcd231cc140d2b9eedb3e4fc5
Matt Auretta <mauretta2@gmail.com>
2018-09-08 14:48:23 -0600

    local strain searches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,76 +18,11 @@ $("#add-strain").on("click", function (event) {
     //Create variable to hold user input
     var strain = $("#strain-input").val().trim()
     //Create local variable to push to Firebase
-    var currentStrain = strain
+    // var currentStrain = strain
     //Set the strain in Firebase
-    database.ref("/strain/").set(currentStrain);
+    // database.ref("/strain/").set(currentStrain);
     //Empty search field
     $("#strain-input").val("")
-})
-
-//function that pushes strains to database
-function masterDatabase() {
-    $.ajax({
-        url: "http://strainapi.evanbusse.com/vij2AV1/strains/search/all",
-        method: "GET",
-
-    }).then(function (response) {
-        // console.log(response)
-        // database.ref("/master/").push({
-        //     strain: 'evan'
-        // });
-        var x;
-        for (x in response) {
-            var strainName = x;
-            var strainId = response[x].id;
-
-            database.ref("/master/").push({
-                strainName: strainName,
-                strainId: strainId
-            });
-        }
-    });
-}
-
-//Only execute to update database
-// masterDatabase();
-
-//Function to create datalist in search bar
-function strainDatalist() {
-    database.ref("/master/").on("value", function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            var master = childSnapshot.val();
-            // console.log(master)
-            var masterArray = [];
-            masterArray.push(master.strainName);
-            // console.log(masterArray);
-            for (var i = 0; i < masterArray.length; i++) {
-                var option = $("<option>").attr("value", masterArray[i]);
-                $("#strains-datalist").append(option);
-            }
-        })
-    })
-}
-
-// Append datalist if strain-input is greater than 3 characters
-// $("#strain-input").on("input", function () {
-//     if ($(this).val().length > 3) {
-//         $("#strains-datalist").empty();
-//         console.log("test")
-//         strainDatalist();
-//     } else {
-//         $("#strains-datalist").empty();
-//     }
-// })
-
-strainDatalist();
-
-//When the databse receives a new strain value
-database.ref("/strain/").on("value", function (childSnapshot) {
-
-    //Create local variable for strain
-    var strain = childSnapshot.val();
-    //AJAX call for specific strain user is searching
     $.ajax({
         url: "http://strainapi.evanbusse.com/vij2AV1/strains/search/name/" + strain + "",
         method: "GET",
@@ -326,4 +261,70 @@ database.ref("/strain/").on("value", function (childSnapshot) {
         console.log(strainChart.data.datasets[6].label);
         
     });
+})
+
+//function that pushes strains to database
+function masterDatabase() {
+    $.ajax({
+        url: "http://strainapi.evanbusse.com/vij2AV1/strains/search/all",
+        method: "GET",
+
+    }).then(function (response) {
+        // console.log(response)
+        // database.ref("/master/").push({
+        //     strain: 'evan'
+        // });
+        var x;
+        for (x in response) {
+            var strainName = x;
+            var strainId = response[x].id;
+
+            database.ref("/master/").push({
+                strainName: strainName,
+                strainId: strainId
+            });
+        }
+    });
+}
+
+//Only execute to update database
+// masterDatabase();
+
+//Function to create datalist in search bar
+function strainDatalist() {
+    database.ref("/master/").on("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            var master = childSnapshot.val();
+            // console.log(master)
+            var masterArray = [];
+            masterArray.push(master.strainName);
+            // console.log(masterArray);
+            for (var i = 0; i < masterArray.length; i++) {
+                var option = $("<option>").attr("value", masterArray[i]);
+                $("#strains-datalist").append(option);
+            }
+        })
+    })
+}
+
+// Append datalist if strain-input is greater than 3 characters
+// $("#strain-input").on("input", function () {
+//     if ($(this).val().length > 3) {
+//         $("#strains-datalist").empty();
+//         console.log("test")
+//         strainDatalist();
+//     } else {
+//         $("#strains-datalist").empty();
+//     }
+// })
+
+strainDatalist();
+
+//When the databse receives a new strain value
+database.ref("/strain/").on("value", function (childSnapshot) {
+
+    //Create local variable for strain
+    var strain = childSnapshot.val();
+    //AJAX call for specific strain user is searching
+    
 });
